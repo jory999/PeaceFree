41b9368a58f2d8b4d53f9de3550f46b0e4926a3d
jory999 <cmdeny21@gmail.com>
2021-04-04 01:40:35 +0800

    aaa

--- boot/main.c
+++ boot/main.c
@@ -9,7 +9,26 @@ extern void FunctionStoreEflags(int eflags);
 
 void InitPalette();
 void DrawRectangle(int x1,int y1,int x2,int y2,int color);
+void init_screen(char *vram, int x, int y);
+
 #define PALETTE_NUMBER 16
+
+#define COL8_000000		0
+#define COL8_FF0000		1
+#define COL8_00FF00		2
+#define COL8_FFFF00		3
+#define COL8_0000FF		4
+#define COL8_FF00FF		5
+#define COL8_00FFFF		6
+#define COL8_FFFFFF		7
+#define COL8_C6C6C6		8
+#define COL8_840000		9
+#define COL8_008400		10
+#define COL8_848400		11
+#define COL8_000084		12
+#define COL8_840084		13
+#define COL8_008484		14
+#define COL8_848484		15
 
 struct BOOTINFO{
        char xyls, leds, vmode, reserve;
@@ -23,16 +42,26 @@ void SysMain()
     char *vram;
     int xsize, ysize;
     struct BOOTINFO *binfo;
-    
-      PutString(20,20,"hello world...\0",0xc6c6c6);
-      PutString(30, 30,"There Is An INT!!!!\0",0xff00ff);
-        
+
+     binfo = (struct BOOTINFO * ) 0x0ff0;
+    xsize = (*binfo).scrnx;
+    ysize = (*binfo).scrny;
+    vram  = (*binfo).vram;
+
     InitPalette();
+    init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
+
+   
+    
+      //PutString(20,20,"hello world...\0",0xc6c6c6);
+      //PutString(30, 30,"There Is An INT!!!!\0",0xff00ff);
+        
+    
     //DrawRectangle(0,0,320,320,9);
     //DrawRectangle(10,10,100,100,10);
     
     DrawRectangle(10,150,300,200,1);
-    DrawRectangle(19,170,90,190,0x840000);
+    DrawRectangle(19,170,90,190,10);
 
     //PutString(20,160,9,"hello world\0");
 
@@ -41,9 +70,8 @@ void SysMain()
 }
 void InitPalette()
 {
-    int i,eflags;
-    static unsigned char table_rgb [PALETTE_NUMBER*3 ] = {
-        0x00, 0x00, 0x00,	/*  0:黑 */
+    static unsigned char table_rgb[16 * 3] = {
+		0x00, 0x00, 0x00,	/*  0:黑 */
 		0xff, 0x00, 0x00,	/*  1:梁红 */
 		0x00, 0xff, 0x00,	/*  2:亮绿 */
 		0xff, 0xff, 0x00,	/*  3:亮黄 */
@@ -59,21 +87,27 @@ void InitPalette()
 		0x84, 0x00, 0x84,	/* 13:暗紫 */
 		0x00, 0x84, 0x84,	/* 14:浅暗蓝 */
 		0x84, 0x84, 0x84	/* 15:暗灰 */
-   };
+	};
+	set_palette(0, 15, table_rgb);
+	return;
 
-    eflags = FunctionLoadEflags();
-    FunctionCli();
-    FunctionOut8(0x03c8,0);
-    for (i=0;i<PALETTE_NUMBER*3;i++)
-    {
-        FunctionOut8(0x03c9,table_rgb[i] / 4 );
-        //FunctionOut8(0x03c9,table_rgb[3*i-3]  );
-        //FunctionOut8(0x03c9,table_rgb[3*i-2]  );
-        //FunctionOut8(0x03c9,table_rgb[3*i-1]  );
-    }
-    FunctionStoreEflags(eflags);
-    //FunctionSti();
-    return;
+	/* C语言中的static char语句只能用于数据，相当于汇编中的DB指令 */
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+	int i, eflags;
+	eflags = FunctionLoadEflags();	/* 记录中断许可标志的值 */
+	FunctionCli(); 					/* 将中断许可标志置为0,禁止中断 */
+	FunctionOut8(0x03c8, start);
+	for (i = start; i <= end; i++) {
+		FunctionOut8(0x03c9, rgb[0] / 4);
+		FunctionOut8(0x03c9, rgb[1] / 4);
+		FunctionOut8(0x03c9, rgb[2] / 4);
+		rgb += 3;
+	}
+	FunctionStoreEflags(eflags);	/* 复原中断许可标志 */
+	return;
 }
 
 void DrawRectangle(int x1,int y1,int x2,int y2,int color)
@@ -94,6 +128,37 @@ void DrawRectangle(int x1,int y1,int x2,int y2,int color)
     }
     return;
 } 
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
+{
+	int x, y;
+	for (y = y0; y <= y1; y++) {
+		for (x = x0; x <= x1; x++)
+			vram[y * xsize + x] = c;
+	}
+	return;
+}
+
+void init_screen(char *vram, int x, int y)
+{
+	boxfill8(vram, x, COL8_008484,  0,     0,      x -  1, y - 29);
+	boxfill8(vram, x, COL8_C6C6C6,  0,     y - 28, x -  1, y - 28);
+	boxfill8(vram, x, COL8_FFFFFF,  0,     y - 27, x -  1, y - 27);
+	boxfill8(vram, x, COL8_C6C6C6,  0,     y - 26, x -  1, y -  1);
+
+	boxfill8(vram, x, COL8_FFFFFF,  3,     y - 24, 59,     y - 24);
+	boxfill8(vram, x, COL8_FFFFFF,  2,     y - 24,  2,     y -  4);
+	boxfill8(vram, x, COL8_848484,  3,     y -  4, 59,     y -  4);
+	boxfill8(vram, x, COL8_848484, 59,     y - 23, 59,     y -  5);
+	boxfill8(vram, x, COL8_000000,  2,     y -  3, 59,     y -  3);
+	boxfill8(vram, x, COL8_000000, 60,     y - 24, 60,     y -  3);
+
+	boxfill8(vram, x, COL8_848484, x - 47, y - 24, x -  4, y - 24);
+	boxfill8(vram, x, COL8_848484, x - 47, y - 23, x - 47, y -  4);
+	boxfill8(vram, x, COL8_FFFFFF, x - 47, y -  3, x -  4, y -  3);
+	boxfill8(vram, x, COL8_FFFFFF, x -  3, y - 24, x -  3, y -  3);
+	return;
+}
  
 
  /* void SysMain()
